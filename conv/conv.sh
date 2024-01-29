e2566de2576982c62223cbd72e625588f642a8c6
fewihu <felix.mueller.97@proton.me>
2024-01-29 18:00:57 +0100

    Add `psig`

---
+++ conv/conv.sh
@@ -0,0 +1,18 @@
+function psig()
+{
+    p=$(ps -ax -ho pid,command | fzf --header="    pid|cmd" | awk '/[:digit]+/ {print $1}')
+    s=$(printf  "SIGINT\nSIGSTOP\nSIGKILL\n" | fzf)
+    echo "sending $s to $p"
+    kill -s "$s" "$p"
+    
+    echo "waiting for $p to die"
+    sleep 3
+
+    ps --pid $p > /dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+	echo "$p still lives"
+    else
+	echo "$p id dead"
+    fi 
+}
