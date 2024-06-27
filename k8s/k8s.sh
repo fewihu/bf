78a92091969daa4d71f76f1a620c534c305a0963
fewihu <felix.mueller.97@proton.me>
2024-06-27 10:42:36 +0200

    Add `un-kc` a function to reset `KUBECONFIG`

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -3,3 +3,11 @@ function k-all-pods
     kubectl get pods -Ao jsonpath='{range .items[*]}{@.metadata.name}{" | "}{@..spec..containers[*].image}{" | "}{@.status.phase}{" | "}{@.status.startTime}{"\n"}{end}'
 
 }
+
+function un-kc
+{
+    oldConfig=$(echo "$KUBECONFIG")
+    unset KUBECONFIG
+    echo "Unset KUBECONFIG: $oldConfig"
+    echo "KUBECONFIG: $KUBECONFIG"
+}
